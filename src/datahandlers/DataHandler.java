a21f9c857d1d0f761863390af8198eb430a30a25
Marcus Larsson <Marcus.b.Larsson@outlook.com>
2022-09-20 21:53:57 +0200

    Added create new file path method

--- src/datahandlers/DataHandler.java
+++ src/datahandlers/DataHandler.java
@@ -9,7 +9,9 @@ class DataHandler {
   Scanner scanner;
   File file;
   FileWriter writer;
-  static String path; 
+  static String path;
+  static String savedListsDir = System.getProperty("user.dir")
+      + "\\src\\savedLists\\";
 
   public DataHandler() {
 
@@ -17,13 +19,39 @@ class DataHandler {
 
   // todo: create exceptions/functions for when the user press cancel.
   static String getSelectedFilePath() {
-    JFileChooser j = new JFileChooser(System.getProperty("user.dir") + "\\src\\savedLists\\");
+    JFileChooser j = new JFileChooser(savedListsDir);
     j.showOpenDialog(null);
 
     path = j.getSelectedFile()
         .getAbsolutePath()
         .replace("\\", "/");
     return path + ".txt";
+  }
+
+  static String CreateNewFilePath() {
+    int newFileNumber = getHighestFileNameNumber(savedListsDir);
+    String newFilePath = String.format(savedListsDir + "myTodo%d.txt", newFileNumber);
+    return newFilePath;
+  }
+
+  static Integer getHighestFileNameNumber(String folderPath) {
+    int currentInt = 1;
+    File dir = new File(folderPath);
+    if (dir.isDirectory()) {
+      for (String fileName : dir.list()) {
+        if (fileName.contains("myTodo")) {
+
+          int fileNr = Integer.parseInt(fileName
+              .replace("myTodo", "") // removes word
+              .replace(".txt", "")); // removes .ext
+
+          if (fileNr >= currentInt) {
+            currentInt = fileNr + 1;
+          }
+        }
+      }
+    }
+    return currentInt ;
   }
 
   static String getCurrentPath() {
